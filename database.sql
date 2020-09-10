12f9202651fa34894effa8ea50cba82d615fbcea
JH-Swin <54096535+JH-Swin@users.noreply.github.com>
2020-09-10 15:27:51 +1000

    Add SQL file to create basic mysql database.

---
+++ database.sql
@@ -0,0 +1,29 @@
+DROP DATABASE PHP_SRePS;
+CREATE DATABASE PHP_SRePS;
+USE PHP_SRePS;
+
+CREATE TABLE items (
+	item_id int(10) AUTO_INCREMENT UNIQUE NOT NULL,
+	item_name varchar(30),
+	PRIMARY KEY (item_id)
+);
+
+CREATE TABLE sales (
+	sale_id int(10) AUTO_INCREMENT UNIQUE,
+	item_id int(10),
+	quantity int(10),
+	PRIMARY KEY (sale_id),
+	FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+
+INSERT INTO items (`item_name`) VALUES ('Panadol');
+INSERT INTO items (`item_name`) VALUES ('Meat');
+INSERT INTO items (`item_name`) VALUES ('Liquid');
+INSERT INTO items (`item_name`) VALUES ('Pain');
+INSERT INTO items (`item_name`) VALUES ('Guck');
+
+INSERT INTO sales (`item_id`, `quantity`) VALUES (1, 10);
+INSERT INTO sales (`item_id`, `quantity`) VALUES (2, 20);
+INSERT INTO sales (`item_id`, `quantity`) VALUES (3, 30);
+INSERT INTO sales (`item_id`, `quantity`) VALUES (4, 40);
+INSERT INTO sales (`item_id`, `quantity`) VALUES (5, 50);
