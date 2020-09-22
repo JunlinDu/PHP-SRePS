fe46ea04ed87dce4dc8d09da965c8f553199b8d2
JH-Swin <102388915@student.swin.edu.au>
2020-09-22 17:23:26 +1000

    exportCSV.py now exports all tables.

--- exportCSV.py
+++ exportCSV.py
@@ -1,7 +1,7 @@
 #Import libraries
-import pymysql #pip install PyMySQL
-import pandas as pd #pip install pandas
-from matplotlib import pyplot as plt #pip install matplotlib
+import pymysql
+import pandas as pd
+
 
 #initialise connection variables
 #https://pythontic.com/database/mysql/query%20a%20table
@@ -12,6 +12,7 @@ dbName = "php_sreps"
 charSet = "utf8mb4"
 cursorType = pymysql.cursors.DictCursor
 
+
 #initialise connection
 connection = pymysql.connect(host=dbServerName, 
     user=dbUser, 
@@ -20,20 +21,27 @@ connection = pymysql.connect(host=dbServerName,
     charset=charSet,
     cursorclass=cursorType)
 
-#body
+
 try:
-    #with connection.cursor() as cursor:
-    query = "select * from items"
-
     #read as dataframe
-    dataframe = pd.read_sql(query, connection)
-
+    batch = pd.read_sql("select * from batch", connection)
+    customer = pd.read_sql("select * from customer", connection)
+    inventory = pd.read_sql("select * from inventory", connection)
+    manufacturer = pd.read_sql("select * from manufacturer", connection)
+    product = pd.read_sql("select * from product", connection)
+    sale_items = pd.read_sql("select * from sale_items", connection)
+    sales = pd.read_sql("select * from sales", connection)
 except Exception as e:
     print("Exception occured:{}".format(e))
 finally:
     connection.close()
 
 
-#export to csv
-    #https://towardsdatascience.com/how-to-export-pandas-dataframe-to-csv-2038e43d9c03
-    #dataframe.to_csv('dataframe.csv', sep='\t')
+#export to csv    #https://towardsdatascience.com/how-to-export-pandas-dataframe-to-csv-2038e43d9c03
+batch.to_csv('CSVs/batch.csv', sep='\t')
+customer.to_csv('CSVs/customer.csv', sep='\t')
+inventory.to_csv('CSVs/inventory.csv', sep='\t')
+manufacturer.to_csv('CSVs/manufacturer.csv', sep='\t')
+product.to_csv('CSVs/product.csv', sep='\t')
+sale_items.to_csv('CSVs/sale_items.csv', sep='\t')
+sales.to_csv('CSVs/sales.csv', sep='\t')
