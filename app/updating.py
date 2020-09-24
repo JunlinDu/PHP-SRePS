70b204540ce0bdfb7bc37e48c1e8b38375a41c44
SimonHzc <54136786+SimonHzc@users.noreply.github.com>
2020-09-24 14:22:41 +1000

    manufacturer change
    a

--- app/updating.py
+++ app/updating.py
@@ -5,6 +5,19 @@ def update_quantity():
     return
 
 
+def update_manufacturer(manufacturer_name,manufacturer_id,cursor):
+    '''
+        Note: This function is able to update the manufacturer of the medicine. This function can be used when changing medicine manufacturers.
+    '''
+    assert type(manufacturer_name) == str
+    assert type(manufacturer_id) == int
+    update = (
+            "update manufacturer set ('Manufacturer_name') where name= ('Manufacturer_id') "
+            "select * from manufacturer where (+ Manufacturer_id +)"
+    )
+    cursor.execute(update)
+    return cursor.fetchone()
+
 
 
 if __name__ == "__main__":
