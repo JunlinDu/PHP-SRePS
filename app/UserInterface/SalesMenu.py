a477ea436bffd899062a9e114a78386b28b3bdd3
JunlinDu <matthewdu7@gmail.com>
2020-10-14 16:53:35 +1100

    comments added for SalesMenu.py

--- app/UserInterface/SalesMenu.py
+++ app/UserInterface/SalesMenu.py
@@ -11,22 +11,35 @@ import connect
 
 connector = connect.conn()
 c = connector.cursor()
+
+# This holds an array of product arrays
 productList = read.table(tables.TableEnum.product, c)
 
 class NewSalesMenu(QMainWindow):
+
+    # This is a hash table that holds individual
+    # added sales (Key: Product Id, Value: Quantity).
+    # It will be added, modified or emptied
+    saleItems  = {}
+
+    # This hash table holds key-value pairs of product Id
+    # and corresponding name. Used for searching for product
+    # name
+    productTable = {}
 
     def __init__(self):
         super(NewSalesMenu, self).__init__()
         loadUi('Pages/SalesWindow.ui', self)
         self.show()
 
-        self.productTable = {}
+        # initiates the productTable table.
         for x in productList:
             self.productTable[x[0]] = x[1]
 
         self.CurrentView = "Sale"
-        self.NewSaleButton.clicked.connect(lambda: self.showAddSaleDialog())
 
+        # Opens a Dialog
+        self.NewSaleButton.clicked.connect(lambda: self.showAddSaleDialog())
 
     def showAddSaleDialog(self):
         dialog = SaleDialog.CreateSaleDialog('Pages/AddSaleDialog.ui', self.productTable)
