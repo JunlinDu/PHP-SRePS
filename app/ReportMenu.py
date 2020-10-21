8c436b5ae0053960bd572de01b8fb2d3890f16fc
kni0002 <edak777@gmail.com>
2020-10-21 20:57:48 +1100

    Add files via upload

---
+++ app/ReportMenu.py
@@ -0,0 +1,115 @@
+# import libraries
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.uic import loadUi
+
+import UserInterface.SideMenuModule as SideMenuModule
+import connect
+import export
+import read
+
+
+class NewReportMenu(QMainWindow):
+    def __init__(self):
+        # initialise variables
+        self.connector = connect.conn()
+        self.cursor = self.connector.cursor()
+        self.CurrentView = "Report"
+        self.startDate = "2020-01-01"
+        self.endDate = "2020-12-31"
+        # initialise window
+        super(NewReportMenu, self).__init__()
+        loadUi('Pages/ReportWindow.ui', self)
+        self.show()
+        # refresh page
+        self.UpdatePage()
+        # setup signals
+        self.monthComboBox.currentIndexChanged.connect(self.UpdatePage)
+        self.yearComboBox.currentIndexChanged.connect(self.UpdatePage)
+        self.ExportReport.clicked.connect(self.Export)
+        SideMenuModule.InitButtons(self, self.ReportMenuButton)
+
+    def ResizeSalesBreakdown(self):
+        # resize breakdown list to better fit.
+        header = self.BreakdownList.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.Stretch)
+
+    def GenerateSalesBreakdown(self):
+        # reset breakdown list
+        self.BreakdownList.clear()
+        self.BreakdownList.setHorizontalHeaderLabels(['Date', 'Product', 'Quantity Sold', 'Total Sales', 'Net Profit'])
+        self.ResizeSalesBreakdown()
+        # refresh variables
+        self.ReadDateBoxes()
+        result = read.sales_breakdown(self.startDate, self.endDate, self.cursor)
+        self.BreakdownList.setRowCount(len(result))
+        # print sales via loop
+        rowCount = 0
+        for item in result:
+            self.BreakdownList.setItem(rowCount, 0, QTableWidgetItem(str(item[0])))
+            self.BreakdownList.setItem(rowCount, 1, QTableWidgetItem(str(item[1])))
+            self.BreakdownList.setItem(rowCount, 2, QTableWidgetItem(str(item[2])))
+            if str(item[3]) == "None":
+                self.BreakdownList.setItem(rowCount, 3, QTableWidgetItem(""))
+                self.BreakdownList.setItem(rowCount, 4, QTableWidgetItem(""))
+            else:
+                self.BreakdownList.setItem(rowCount, 3, QTableWidgetItem("$" + str(item[3])))
+                self.BreakdownList.setItem(rowCount, 4, QTableWidgetItem("$" + str(item[4])))
+            rowCount = rowCount + 1
+
+    def GenerateReportElements(self):
+        # refresh variables
+        self.ReadDateBoxes()
+        # read/select first row of report_elements
+        result = read.report_elements(self.startDate, self.endDate, self.cursor)[0]
+        # write results to ui elements
+        if str(result[0]) == "None":
+            self.TotalSales.setText("$0")
+            self.NetProfit.setText("$0")
+        else:
+            self.TotalSales.setText("$" + str(result[0]))
+            self.NetProfit.setText("$" + str(result[1]))
+        self.NumberOfSales.setText(str(result[2]))
+        self.NumberOfProducts.setText(str(result[3]))
+
+    def UpdatePage(self):
+        self.GenerateReportElements()
+        self.GenerateSalesBreakdown()
+
+    def ReadDateBoxes(self):
+        # read month
+        month = int(self.monthComboBox.currentIndex())
+        if month == 0:
+            startMonth = "01"
+            endMonth = "12"
+        else:
+            startMonth, endMonth = str('{:02}'.format(month)), str('{:02}'.format(month))
+        # read year
+        year = self.yearComboBox.currentText()
+        if year == "ALL DATA":
+            self.monthComboBox.setCurrentText("Any")
+            startYear = "0000"
+            endYear = "9999"
+        else:
+            startYear, endYear = year, year
+        # update date fields
+        self.startDate = startYear + "-" + startMonth + "-01"
+        self.endDate = endYear + "-" + endMonth + "-31"
+
+    def Export(self):
+        result = read.sales_breakdown(self.startDate, self.endDate, self.cursor)
+        export.result(result)
+        # implement popup?
+
+
+# testing
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = NewReportMenu()
+    sys.exit(app.exec_())
---
+++ app/SalesMenu.py
@@ -0,0 +1,234 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QTableWidgetItem, QDialog, QTableView, QTableWidget
+from PyQt5.uic import loadUi
+from UserInterface import SaleDialog
+from datetime import datetime
+import read
+import tables
+import connect
+import insert
+import UserInterface.SideMenuModule as SideMenuModule
+
+connector = connect.conn()
+c = connector.cursor()
+
+# This variable holds an array of product arrays
+productList = read.table(tables.TableEnum.product, c)
+
+# This variable holds an array of customers
+customerList = read.table(tables.TableEnum.customer, c)
+
+
+class NewSalesMenu(QMainWindow):
+    # This is a hash table that holds individual
+    # added sales (Key: Product Id, Value: Quantity).
+    # It will be added, modified or emptied
+    saleItems = {}
+
+    # This hash table holds key-value pairs of products
+    # Id and corresponding name. Used for searching for
+    # product name
+    productTable = {}
+
+    # Customer table, used to verify that the given
+    # customer Id correspond to a registered customer
+    customerTable = {}
+
+    # aggregated total price
+    total = 0.0
+
+    def __init__(self):
+        super(NewSalesMenu, self).__init__()
+        loadUi('Pages/SalesWindow.ui', self)
+        self.show()
+        self.CurrentView = "Sale"
+
+        self.initiateTables()
+        self.setDate(datetime.today())
+        SideMenuModule.InitButtons(self, self.SalesMenuButton)
+
+        header = self.SaleList.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+
+        self.SaleList.setSelectionBehavior(QTableView.SelectRows)
+
+        self.Submit.clicked.connect(lambda: self.submit())
+        self.Cancel.clicked.connect(lambda: self.initialize())
+
+        self.NewSaleButton.clicked.connect(lambda: self.showAddSaleDialog(None, None))
+        self.EditSalesItem.clicked.connect(lambda: self.editEntry())
+        self.DeleteSalesItem.clicked.connect(lambda: self.deleteEntry())
+
+    def editEntry(self):
+        selectedrow = self.SaleList.selectionModel().selectedRows()
+        for index in sorted(selectedrow):
+            print(index)
+
+        # for index in sorted(a):
+        #     print(index.row())
+        return
+
+    def deleteEntry(self):
+        return
+
+    def initiateTables(self):
+        # initiates the productTable table.
+        for x in productList:
+            self.productTable[x[0]] = [x[1], x[3]]
+        # initiates the customerTable table.
+        for c in customerList:
+            self.customerTable[c[0]] = (c[1] + " " + c[2])
+        # print(self.productTable)
+        # print(self.customerTable)
+
+    def setSaleItems(self, dialog):
+        if dialog.producttuple != (0, 0):
+            if dialog.producttuple[0] not in self.saleItems:
+                self.saleItems[dialog.producttuple[0]] = dialog.producttuple[1]
+            else:
+                self.saleItems[dialog.producttuple[0]] = self. \
+                                                             saleItems[dialog.producttuple[0]] + dialog.producttuple[1]
+            self.setColumns()
+
+    def setDate(self, date):
+        d = QDate(date.year, date.month, date.day)
+        self.dateEdit.setDate(d)
+        # print(type(self.dateEdit.date().day()))
+
+    def setColumns(self):
+        self.SaleList.setRowCount(len(self.saleItems))
+        rowNumber = 0
+        for PId in self.saleItems:
+            quantity = self.saleItems[PId]
+            subtotal = float(self.productTable[PId][1] * quantity) * 1.2
+
+            self.total += subtotal
+            self.SaleList.setItem(rowNumber, 0, QTableWidgetItem(str(PId)))
+            self.SaleList.setItem(rowNumber, 1, QTableWidgetItem(self.productTable[PId][0]))
+            self.SaleList.setItem(rowNumber, 2, QTableWidgetItem(str(quantity)))
+            self.SaleList.setItem(rowNumber, 3, QTableWidgetItem(str(subtotal)))
+
+            rowNumber += 1
+
+        self.GrandTotalText.setText(str("{:.2f}".format(self.total)))
+        self.NetProfitText.setText(str("{:.2f}".format(self.total - self.total * 0.8)))
+
+    def initialize(self):
+        self.saleItems = {}
+        self.total = 0
+        self.setDate(datetime.today())
+        self.GrandTotalText.setText("0")
+        self.NetProfitText.setText("0")
+        self.SaleList.clear()
+        self.SaleList.setRowCount(0)
+        self.SaleList.setHorizontalHeaderLabels(['Quantity', 'Product Name', 'Quantity', 'Sub Total'])
+
+    def submit(self):
+        date = self.dateEdit.date()
+        customerId = self.lineEditId.text()
+        total = self.GrandTotalText.text()
+
+        if not self.verifyCustomerId(customerId):
+            return
+        if len(self.saleItems) == 0:
+            self.showMessageDialog("Sales List is Empty, Please Enter Items Before Proceed")
+        self.showSummaryDialog(date, customerId,
+                               self.customerTable[int(customerId)],
+                               total, self.saleItems, self.productTable)
+
+
+    def verifyCustomerId(self, id):
+        content = self.lineEditId.text()
+        if content == "" or not content.isnumeric():
+            self.showMessageDialog("Please Enter a Valid Numeric Customer ID")
+            return False
+        elif self.customerTable[int(content)] is None:
+            self.showMessageDialog("The ID Entered Has no Corresponding Entry in the Database")
+            return False
+        return True
+
+    def saleConfirm(self, dialog):
+        if dialog.productSoldOut is None:
+            self.initialize()
+        else:
+            self.showMessageDialog("There is not enough "+dialog.productSoldOut + " left")
+
+    def showAddSaleDialog(self, prodId, prodQuantity):
+        dialog = SaleDialog.CreateSaleDialog('Pages/AddSaleDialog.ui', self.productTable, prodId, prodQuantity)
+        dialog.buttonBox.accepted.connect(lambda: self.setSaleItems(dialog))
+        dialog.exec_()
+
+    def showMessageDialog(self, message):
+        dialog = MessageDialog(message)
+        dialog.exec_()
+
+    def showSummaryDialog(self, date, customerId, name, total, saleItemsTable, productsTable):
+        dialog = Summary(date, customerId, name, total, saleItemsTable, productsTable)
+        dialog.buttonBox.accepted.connect(lambda: self.saleConfirm(dialog))
+        dialog.exec_()
+
+class Summary(QDialog):
+
+    productSoldOut = None
+
+    def __init__(self, date, customerId, name, total, saleItemsTable, productsTable):
+        super(Summary, self).__init__()
+        loadUi('Pages/SummaryDialog.ui', self)
+
+        self.date = str(date.year()) + "-" + str(date.month()) + "-" + str(date.day())
+        self.customerId = customerId
+        self.name = name
+        self.total = total
+        self.saleItems = saleItemsTable
+        self.productsTable = productsTable
+
+        self.setSaleList()
+
+        self.Id.setText(str(self.customerId))
+        self.Name.setText(self.name)
+        self.Date.setText(datetime.strptime(self.date, '%Y-%m-%d').strftime('%m/%d/%Y'))
+        self.Total.setText(str(total))
+
+        # self.buttonBox.accepted.connect(self.accept)
+        self.accepted.connect(lambda: self.execute())
+
+    def execute(self):
+        arr = []
+        for PId in self.saleItems:
+            arr.append((PId, self.saleItems[PId]))
+        print(arr)
+        id = insert.new_sale(self.date, connector, c, int(self.customerId), arr)
+        if type(id) is not int:
+            self.productSoldOut = id.split(' ', 1)[0]
+        self.close()
+
+    def setSaleList(self):
+        self.SaleList.setRowCount(len(self.saleItems))
+        r = 0
+
+        for PId in self.saleItems:
+            self.SaleList.setItem(r, 0, QTableWidgetItem(self.productsTable[PId][0]))
+            self.SaleList.setItem(r, 1, QTableWidgetItem(str(self.saleItems[PId])))
+            r += 1
+
+
+class MessageDialog(QDialog):
+
+    def __init__(self, message):
+        assert type(message) == str
+        super(MessageDialog, self).__init__()
+        loadUi('Pages/MessageDialog.ui', self)
+        self.message.setText(message)
+
+
+# init
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = NewSalesMenu()
+    sys.exit(app.exec_())
